cac6511aeb172052a08a2847dac9af48c03539b7
Lumiere Xyloto <740207968@qq.com>
2019-02-02 23:21:05 +0800

    finish app tab component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="app">
-    hello music
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MHeader,
+    Tab
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" scoped>
+// 前面加 ~ 表示你后面的值为 alias, 然后就会去 webpack alias 配置中找相应的值, 然后拼接成最后的地址
+@import "~common/stylus/variable"
+#app
+  color $color-theme
 </style>
