34f67d66142bb4f447a58e242716bc7cc6483dfb
Lumiere Xyloto <740207968@qq.com>
2019-02-03 17:22:06 +0800

    完成better-scroll轮播

---
+++ src/common/js/dom.js
@@ -0,0 +1,14 @@
+// 为dom添加类名
+export function addClass(el, className) {
+  if (hasClass(el, className)) {
+    return
+  }
+  let newClass = el.className.split(' ')
+  newClass.push(className)
+  el.className = newClass.join(' ')
+}
+
+export function hasClass(el, className) {
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+  return reg.test(el.className)
+}
